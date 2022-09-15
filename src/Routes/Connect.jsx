e7616f757c5cd80ff0017ff7d7bbbcd6f1560f37
Ji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-09-15 21:51:55 +0900

    12-1 디자인 오류해결

--- src/Routes/Connect.jsx
+++ src/Routes/Connect.jsx
@@ -1,338 +1,336 @@
 import styled from "styled-components";
-import Nav from "./components/Nav"
-import { HomeDiv} from "./Home";
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import Nav from "./components/Nav";
+import { HomeDiv } from "./Home";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import { useState } from "react";
 import Footer from "./components/Footer";
 
 const ConnectDiv = styled.section`
-    width:90%;
-    margin: 0 auto;
-    align-items: center;
-    display: flex;
-    justify-content:space-around;
-    grid-template-columns: 1fr;
-    @media all and (min-width:480px) and (max-width:767px) {
-       flex-direction: column;
-       justify-content:center;
-    } 
-    @media all and (max-width:479px) {
-        flex-direction :column;
-        justify-content:center;
-    }
-`
+  width: 70%;
+  max-width: 1440px;
+  margin: 0 auto;
+  align-items: center;
+  display: flex;
+  justify-content: space-around;
+  grid-template-columns: 1fr;
+  @media all and (min-width: 480px) and (max-width: 767px) {
+    flex-direction: column;
+    justify-content: center;
+  }
+  @media all and (max-width: 479px) {
+    flex-direction: column;
+    justify-content: center;
+  }
+`;
 
 const LeftDiv = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width:50%;
-    margin: 0;
-    @media all and (min-width:480px) and (max-width:767px) {
-      margin: 0;
-    } 
-    @media all and (max-width:479px) {
-        margin: 0;
-    }
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30%;
+  margin: 0;
+  @media all and (max-width: 767px) {
+    width: 100%;
+  }
+`;
 
 const RightDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    width:50%;
-    
-    margin : 0;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  width: 70%;
+  margin: 0;
+  @media all and (max-width: 767px) {
+    width: 100%;
+  }
+`;
 const NameH1 = styled.div`
-    font-size:48px;
-    font-weight: bold;
-    margin: 20px 0px;
-    opacity: 0.6;
-    @media all and (min-width:480px) and (max-width:767px) {
-        font-size: 24px;
-    } 
-    @media all and (max-width:479px) {
-        font-size: 24px;
-    }
-`
+  font-size: 48px;
+  font-weight: bold;
+  margin: 20px 0px;
+  opacity: 0.6;
+  @media all and (min-width: 480px) and (max-width: 767px) {
+    font-size: 24px;
+  }
+  @media all and (max-width: 479px) {
+    font-size: 24px;
+  }
+`;
 const NameH3 = styled.div`
-    opacity: 0.6;
-    font-size:18px;
-    margin: 20px 0px;
-    @media all and (min-width:480px) and (max-width:767px) {
-        font-size: 10px;
-    } 
-    @media all and (max-width:479px) {
-        font-size: 10px;
-    }
-`
+  opacity: 0.6;
+  font-size: 18px;
+  margin: 20px 0px;
+  @media all and (min-width: 480px) and (max-width: 767px) {
+    font-size: 10px;
+  }
+  @media all and (max-width: 479px) {
+    font-size: 10px;
+  }
+`;
 
-const ContactCommit= styled.div`
-    margin:40px 0px;
-    opacity: 0.6;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    @media all and (min-width:480px) and (max-width:767px) {
-        margin:10px 0px;
-    } 
-    @media all and (max-width:479px) {
-        margin:10px 0px;
-    }
-`
-const ContacktCommit1=styled.div`
-    font-size:20px;
+const ContactCommit = styled.div`
+  margin: 40px 0px;
+  opacity: 0.6;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  @media all and (min-width: 480px) and (max-width: 767px) {
     margin: 10px 0px;
-    @media all and (min-width:480px) and (max-width:767px) {
-        font-size: 10px;
-    } 
-    @media all and (max-width:479px) {
-        font-size: 10px;
-    }
-`
+  }
+  @media all and (max-width: 479px) {
+    margin: 10px 0px;
+  }
+`;
+const ContacktCommit1 = styled.div`
+  font-size: 20px;
+  margin: 10px 0px;
+  @media all and (min-width: 480px) and (max-width: 767px) {
+    font-size: 10px;
+  }
+  @media all and (max-width: 479px) {
+    font-size: 10px;
+  }
+`;
 const ContactEmail = styled.div`
-    font-size:16px;
-    cursor: pointer;
-    :hover{
-        transform: scale(1.05);
-    }
-    @media all and (min-width:480px) and (max-width:767px) {
-        font-size: 10px;
-    } 
-    @media all and (max-width:479px) {
-        font-size: 10px;
-    }
-`
-const BtnDiv= styled.div`
-    width:200px;
-    margin:50px 0px;
-    height:50px;
-    display: flex;
-    justify-content: flex-end;
-`
+  font-size: 16px;
+  cursor: pointer;
+  :hover {
+    transform: scale(1.05);
+  }
+  @media all and (min-width: 480px) and (max-width: 767px) {
+    font-size: 10px;
+  }
+  @media all and (max-width: 479px) {
+    font-size: 10px;
+  }
+`;
+const BtnDiv = styled.div`
+  width: 200px;
+  margin: 50px 0px;
+  height: 50px;
+  display: flex;
+  justify-content: flex-end;
+`;
 
 const HelloBtn = styled.div`
-    font-size : 24px;
-    margin:20px 0px 0px 0px;
-    transition: transform 250ms;
-    cursor: pointer;
-    :hover{
-        transform: translateY(-3px);
-    }
-    @media all and (min-width:480px) and (max-width:767px) {
-        font-size: 10px;
-    } 
-    @media all and (max-width:479px) {
-        font-size: 10px;
-    }
-`
+  font-size: 24px;
+  margin: 20px 0px 0px 0px;
+  transition: transform 250ms;
+  cursor: pointer;
+  :hover {
+    transform: translateY(-3px);
+  }
+  @media all and (min-width: 480px) and (max-width: 767px) {
+    font-size: 10px;
+  }
+  @media all and (max-width: 479px) {
+    font-size: 10px;
+  }
+`;
 
-const Slash =styled.div`
-    font-size : 24px;
-    margin: 20px 10px 0px 0px;
-    @media all and (min-width:480px) and (max-width:767px) {
-        font-size: 10px;
-    } 
-    @media all and (max-width:479px) {
-        font-size: 10px;
-    }
-`
+const Slash = styled.div`
+  font-size: 24px;
+  margin: 20px 10px 0px 0px;
+  @media all and (min-width: 480px) and (max-width: 767px) {
+    font-size: 10px;
+  }
+  @media all and (max-width: 479px) {
+    font-size: 10px;
+  }
+`;
 
 const DropText = styled.div`
-    display: inline-block;
-    width: 180px;
-    text-align: left;
-    height: 200px;
-    margin-right: 30px;
-    font-size: 48px;
-    vertical-align: -2px;
-    @media all and (min-width:480px) and (max-width:767px) {
-        font-size: 28px;
-        font-weight: bold;
-        height: 100px;
-    } 
-    @media all and (max-width:479px) {
-        font-size: 28px;
-        font-weight: bold;
-        height: 100px;
-    }
-`
+  display: inline-block;
+  width: 180px;
+  text-align: left;
+  height: 200px;
+  margin-right: 30px;
+  font-size: 48px;
+  vertical-align: -2px;
+  @media all and (min-width: 480px) and (max-width: 767px) {
+    font-size: 28px;
+    font-weight: bold;
+    height: 100px;
+  }
+  @media all and (max-width: 479px) {
+    font-size: 28px;
+    font-weight: bold;
+    height: 100px;
+  }
+`;
 
-const DropItem =styled.div`
-    font-size:0px;
-    opacity:0;
-    margin-left:-30px;
-    position:absolute;
-    font-weight:300;   
-    
-    :nth-child(1) {
-        animation: roll 5s linear infinite 0s;
+const DropItem = styled.div`
+  font-size: 0px;
+  opacity: 0;
+  margin-left: -30px;
+  position: absolute;
+  font-weight: 300;
+  :nth-child(1) {
+    animation: roll 5s linear infinite 0s;
+  }
+  :nth-child(2) {
+    animation: roll 5s linear infinite 1s;
+  }
+  :nth-child(3) {
+    animation: roll 5s linear infinite 2s;
+  }
+  :nth-child(4) {
+    animation: roll2 5s linear infinite 3s;
+  }
+  @keyframes roll {
+    0% {
+      font-size: 0px;
+      opacity: 0;
+      margin-left: -30px;
+      margin-top: 0px;
+      transform: rotate(-25deg);
     }
-    :nth-child(2) {
-        animation: roll 5s linear infinite 1s;
+    3% {
+      opacity: 1;
+      transform: rotate(0deg);
     }
-    :nth-child(3) {
-        animation: roll 5s linear infinite 2s;
+    5% {
+      font-size: inherit;
+      opacity: 1;
+      margin-left: 0px;
+      margin-top: 0px;
     }
-    :nth-child(4) {
-        animation: roll2 5s linear infinite 3s;
+    20% {
+      font-size: inherit;
+      opacity: 1;
+      margin-left: 0px;
+      margin-top: 0px;
+      transform: rotate(0deg);
     }
-@keyframes roll {
-  0% {
-    font-size:0px;
-    opacity:0;
-    margin-left:-30px;
-    margin-top:0px;
-    transform: rotate(-25deg);
+    27% {
+      font-size: 0px;
+      opacity: 0.5;
+      margin-left: 20px;
+      margin-top: 100px;
+    }
+    100% {
+      font-size: 0px;
+      opacity: 0;
+      margin-left: -30px;
+      margin-top: 0px;
+      transform: rotate(15deg);
+    }
   }
-  3% {
-    opacity:1;
-    transform: rotate(0deg);
-  }
-  5% {
-    font-size:inherit;
-    opacity:1;
-    margin-left:0px;
-    margin-top:0px;
-  }
-  20% {
-    font-size:inherit;
-    opacity:1;
-    margin-left:0px;
-    margin-top:0px;
-    transform: rotate(0deg);
-  }
-  27% {
-    font-size:0px;
-    opacity:0.5;
-    margin-left:20px;
-    margin-top:100px;
-  }
-  100% {
-    font-size:0px;
-    opacity:0;
-    margin-left:-30px;
-    margin-top:0px;
-    transform: rotate(15deg);
-  }
-}
 
-@keyframes roll2 {
-  0% {
-    font-size:0px;
-    opacity:0;
-    margin-left:-30px;
-    margin-top:0px;
-    transform: rotate(-25deg);
+  @keyframes roll2 {
+    0% {
+      font-size: 0px;
+      opacity: 0;
+      margin-left: -30px;
+      margin-top: 0px;
+      transform: rotate(-25deg);
+    }
+    3% {
+      opacity: 1;
+      transform: rotate(0deg);
+    }
+    5% {
+      font-size: inherit;
+      opacity: 1;
+      margin-left: 0px;
+      margin-top: 0px;
+    }
+    30% {
+      font-size: inherit;
+      opacity: 1;
+      margin-left: 0px;
+      margin-top: 0px;
+      transform: rotate(0deg);
+    }
+    37% {
+      font-size: 0px;
+      opacity: 0.5;
+      margin-left: 20px;
+      margin-top: 100px;
+    }
+
+    100% {
+      font-size: 0px;
+      opacity: 0;
+      margin-left: -30px;
+      margin-top: 0px;
+      transform: rotate(15deg);
+    }
   }
-  3% {
-    opacity:1;
-    transform: rotate(0deg);
-  }
-  5% {
-    font-size:inherit;
-    opacity:1;
-    margin-left:0px;
-    margin-top:0px;
-  }
-  30% {
-    font-size:inherit;
-    opacity:1;
-    margin-left:0px;
-    margin-top:0px;
-    transform: rotate(0deg);
-  }
-  37% {
-    font-size:0px;
-    opacity:0.5;
-    margin-left:20px;
-    margin-top:100px;
-  }
-  
-  100% {
-    font-size:0px;
-    opacity:0;
-    margin-left:-30px;
-    margin-top:0px;
-    transform: rotate(15deg);
-  }
-}
-`
+`;
 
 const DropTitle = styled.div`
-    font-size:24px;
-    @media all and (min-width:480px) and (max-width:767px) {
-        width:600px;
-        font-size: 20px;
-    } 
-    @media all and (max-width:479px) {
-        width:600px;
-        font-size: 20px;
-    }
-    
-`
+  font-size: 24px;
+  width: 100%;
+  @media all and(max-width: 767px) {
+    width: 100%;
+    font-size: 20px;
+  }
+`;
 
-function Connect(){ 
-    const [isCopy , setIsCopy] = useState(false);
-    const onCopy=()=>{
-        setIsCopy(true)
-    }
-    return  (
-        <>
-        <Nav/>
-        <HomeDiv
-            transition= { {duration: 0.5 }}
-            initial= { {opacity: 0, y: 400 }}
-            animate= { {opacity: 1, y: 0 }}
-            exit= { {opacity: 0, y: -400} }  
-        >
+function Connect() {
+  const [isCopy, setIsCopy] = useState(false);
+  const onCopy = () => {
+    setIsCopy(true);
+  };
+  return (
+    <>
+      <Nav />
+      <HomeDiv
+        transition={{ duration: 0.5 }}
+        initial={{ opacity: 0, y: 400 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -400 }}
+      >
         <ConnectDiv>
-            <LeftDiv>
-                <DropText>
-                    <DropItem>책임감을 가진</DropItem> 
-                    <DropItem>할 일을 미루지 않는</DropItem>
-                    <DropItem>소통할줄 아는</DropItem>
-                    <DropItem>배움의 의지가 강한</DropItem>
-                </DropText>
-                <DropTitle>
-                이지훈 입니다
-                </DropTitle>
-            </LeftDiv>
-            <RightDiv>
-                <NameH1>JIHOON LEE</NameH1>
-                <NameH3>Front-End Develope</NameH3>
-                <ContactCommit>
-                    <ContacktCommit1>
-                    If you need to talk to me
-                    </ContacktCommit1>
-                    <ContacktCommit1>
-                    Please email to
-                    </ContacktCommit1>
-                </ContactCommit>
-                <CopyToClipboard text="jihoon7705@gmail.com">
-                    <ContactEmail onClick={onCopy}>
-                        {isCopy ? "이메일이 복사 되었습니다" : "jihoon7705@gmail.com"}
-                        
-                    </ContactEmail>
-                </CopyToClipboard>
-                <BtnDiv style={{justifyItems:"flex-end"}}>
-                        <HelloBtn style>
-                            <a href="https://www.notion.so/Full-Stack-Developer-52ce9ef0ba8d4a3583ac46a5787dd888"  rel="noopener noreferrer" target="_blank">NOTION</a> 
-                        </HelloBtn>
-                        <Slash>
-                        ｜
-                        </Slash>
-                        <HelloBtn style>
-                            <a href="https://github.com/jiji-hoon96"  rel="noopener noreferrer" target="_blank">GITHUB</a>
-                        </HelloBtn>
-                    </BtnDiv>
-            </RightDiv>
+          <LeftDiv>
+            <DropText>
+              <DropItem>계속해서 배우고</DropItem>
+              <DropItem>계속해서 고민하고</DropItem>
+              <DropItem>오늘보다 내일이</DropItem>
+              <DropItem>아름다운</DropItem>
+            </DropText>
+            <DropTitle>이지훈</DropTitle>
+          </LeftDiv>
+          <RightDiv>
+            <NameH1>JIHOON LEE</NameH1>
+            <NameH3>Front-End Develope</NameH3>
+            <ContactCommit>
+              <ContacktCommit1>If you need to talk to me</ContacktCommit1>
+              <ContacktCommit1>Please email to</ContacktCommit1>
+            </ContactCommit>
+            <CopyToClipboard text="jihoon7705@gmail.com">
+              <ContactEmail onClick={onCopy}>
+                {isCopy ? "이메일이 복사 되었습니다" : "jihoon7705@gmail.com"}
+              </ContactEmail>
+            </CopyToClipboard>
+            <BtnDiv style={{ justifyItems: "flex-end" }}>
+              <HelloBtn style>
+                <a
+                  href="https://www.notion.so/Full-Stack-Developer-52ce9ef0ba8d4a3583ac46a5787dd888"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  NOTION
+                </a>
+              </HelloBtn>
+              <Slash>｜</Slash>
+              <HelloBtn style>
+                <a
+                  href="https://github.com/jiji-hoon96"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  GITHUB
+                </a>
+              </HelloBtn>
+            </BtnDiv>
+          </RightDiv>
         </ConnectDiv>
-        </HomeDiv>
-        <Footer/>
-        </>
-    )
+      </HomeDiv>
+      <Footer />
+    </>
+  );
 }
 
-export default Connect
+export default Connect;
--- src/Routes/components/Nav.jsx
+++ src/Routes/components/Nav.jsx
@@ -1,167 +1,171 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import NavToggle from './NavToggle';
-import { Sun,Moon } from "react-feather";
+import NavToggle from "./NavToggle";
+import { Sun, Moon } from "react-feather";
 
 const NavbarDiv = styled.div`
-    width:98%;
-    position: fixed;
-    display: flex;
-    height: 100px;
-    top: 0;
-    z-index: 10;
-    opacity: 0.97;
-    background-color: ${(props)=>props.theme.bgColor};
-`
+  width: 100%;
+  min-width: 300px;
+  position: fixed;
+  display: flex;
+  height: 100px;
+  margin: 0 auto;
+  top: 0;
+  z-index: 10;
+  opacity: 0.97;
+  background-color: ${(props) => props.theme.bgColor};
+`;
 
-const LogoDiv= styled.div`
-    width:30%;
-    height: 100px;
-    margin-right: 20px;
-`
+const LogoDiv = styled.div`
+  width: 40%;
+  height: 100px;
+  margin-right: 20px;
+  @media all and (max-width: 500px) {
+    width: 20%;
+  }
+`;
 const NavItemDiv = styled.div`
-    width:70%;
-    height: 100px;
-    display:flex;
-    justify-content: center;
-    align-items: center;
-`
+  width: 60%;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media all and (max-width: 500px) {
+    width: 80%;
+  }
+`;
 
-const NavItemEach =styled.div`
-    display:flex;
-    align-items: center;
-    justify-content: center;
-    margin: 0px 10px 0px 0px;
-    font-size:20px;
-    font-weight: bold;
-    width: 150px;
-    height: 60px;
-    cursor: pointer;
-    transition: transform 250ms;
-    :hover{
-        transform: translateY(-3px);
-    }
-    @media all and (min-width:480px) and (max-width:767px) {
-        width:15%;
-        font-size: 16px;
-        margin:0px 2%;
-    } 
-    @media all and (max-width:479px) {
-        width:15%;
-        margin:0px 2%;
-        font-size: 16px;
-    }
-`
+const NavItemEach = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 10px 0px 0px;
+  font-size: 20px;
+  font-weight: bold;
+  width: 150px;
+  height: 60px;
+  cursor: pointer;
+  transition: transform 250ms;
+  :hover {
+    transform: translateY(-3px);
+  }
+  @media all and (min-width: 480px) and (max-width: 767px) {
+    width: 15%;
+    font-size: 16px;
+    margin: 0px 2%;
+  }
+  @media all and (max-width: 479px) {
+    width: 15%;
+    margin: 0px 2%;
+    font-size: 16px;
+  }
+
+  @media all and (max-width: 350px) {
+    font-size: 14px;
+  }
+`;
 
 const SpanEach = styled.span`
-    position: absolute;
-    top:25%;
-    left:11%;
-    width:22px;
-    height: 22px;
-    border-radius: 50%;
-    animation: speed 2s infinite ease-in-out;
-    @keyframes speed {
+  position: absolute;
+  top: 25%;
+  left: 11%;
+  width: 22px;
+  height: 22px;
+  border-radius: 50%;
+  animation: speed 2s infinite ease-in-out;
+  @keyframes speed {
     0% {
-        transform: translate(0%);
-        border-radius: 50%;
+      transform: translate(0%);
+      border-radius: 50%;
     }
 
     25% {
-        transform: translate(150%) scale(0.5);
-        border-radius: 0%;
+      transform: translate(150%) scale(0.5);
+      border-radius: 0%;
     }
 
     50% {
-        transform: translate(150%, 150%);
-        border-radius: 50%;
+      transform: translate(150%, 150%);
+      border-radius: 50%;
     }
 
     75% {
-        transform: translate(0, 150%) scale(0.5);
-        border-radius: 0%;
+      transform: translate(0, 150%) scale(0.5);
+      border-radius: 0%;
     }
-    }
-    @media all and (min-width:480px) and (max-width:767px) {
-        width:12px;
-        height: 12px;
-        top:35%;
-        left:11%;
-    } 
-    @media all and (max-width:479px) {
-        width:12px;
-        height: 12px;
-        top:35%;
-        left:11%;
-    }
-`
+  }
+  @media all and (min-width: 480px) and (max-width: 767px) {
+    width: 12px;
+    height: 12px;
+    top: 35%;
+    left: 11%;
+  }
+  @media all and (max-width: 479px) {
+    width: 12px;
+    height: 12px;
+    top: 35%;
+    left: 11%;
+  }
+`;
 
-const SpanOne =styled(SpanEach)`
-    background: ${(props)=>props.theme.navItemColorOne};
-    animation-delay: 1.5s;
-`
+const SpanOne = styled(SpanEach)`
+  background: ${(props) => props.theme.navItemColorOne};
+  animation-delay: 1.5s;
+`;
 const SpanTwo = styled(SpanEach)`
-    font-size:32px;
-    animation-delay: 1s;
-    @media all and (min-width:480px) and (max-width:767px) {
-        font-size:20px;
-    } 
-    @media all and (max-width:479px) {
-        font-size:20px;
-    }
-`
+  font-size: 32px;
+  animation-delay: 1s;
+  @media all and (min-width: 480px) and (max-width: 767px) {
+    font-size: 20px;
+  }
+  @media all and (max-width: 479px) {
+    font-size: 20px;
+  }
+`;
 const SpanThree = styled(SpanEach)`
-    background: ${(props)=>props.theme.navItemColortwo};
-    animation-delay: 0.5s;
-`
+  background: ${(props) => props.theme.navItemColortwo};
+  animation-delay: 0.5s;
+`;
 
 const SpanFour = styled(SpanEach)`
-    font-size:32px;
-    @media all and (min-width:480px) and (max-width:767px) {
-        font-size:20px;
-    } 
-    @media all and (max-width:479px) {
-        font-size:20px;
-    }
-`
+  font-size: 32px;
+  @media all and (min-width: 480px) and (max-width: 767px) {
+    font-size: 20px;
+  }
+  @media all and (max-width: 479px) {
+    font-size: 20px;
+  }
+`;
 
-function Nav(){
-    return (
-        <NavbarDiv>
-            <LogoDiv>
-                <SpanOne/>
-                <SpanTwo>J</SpanTwo>
-                <SpanThree/>
-                <SpanFour>H</SpanFour>
-            </LogoDiv>
-            <NavItemDiv>
-                <NavItemEach>
-                    <Link to="/">
-                        HOME
-                    </Link>
-                </NavItemEach>
-                <NavItemEach>
-                    <Link to="/project">
-                        PROJECT
-                    </Link>
-                </NavItemEach>
-                <NavItemEach>
-                    <Link to="/about">
-                        SKILL
-                    </Link>
-                </NavItemEach>
-                <NavItemEach>
-                    <Link to="/connect">
-                        CONNECT
-                    </Link>
-                </NavItemEach>
-                <NavToggle>
-                    <Sun />
-                    <Moon />
-			    </NavToggle>
-            </NavItemDiv>
-        </NavbarDiv>
-    )
+function Nav() {
+  return (
+    <NavbarDiv>
+      <LogoDiv>
+        <SpanOne />
+        <SpanTwo>J</SpanTwo>
+        <SpanThree />
+        <SpanFour>H</SpanFour>
+      </LogoDiv>
+      <NavItemDiv>
+        <NavItemEach>
+          <Link to="/">HOME</Link>
+        </NavItemEach>
+        <NavItemEach>
+          <Link to="/project">PROJECT</Link>
+        </NavItemEach>
+        <NavItemEach>
+          <Link to="/about">SKILL</Link>
+        </NavItemEach>
+        <NavItemEach>
+          <Link to="/connect">CONNECT</Link>
+        </NavItemEach>
+        <NavToggle>
+          <Sun />
+          <Moon />
+        </NavToggle>
+      </NavItemDiv>
+    </NavbarDiv>
+  );
 }
 
 export default Nav;
