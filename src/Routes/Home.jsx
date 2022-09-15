fdc77fcae455c9e7d9e9e6c8272aa182a0386db0
Ji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-09-15 22:38:39 +0900

    12-2 프로젝트 툴생성

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,162 +1,172 @@
 import Nav from "./components/Nav";
 import styled from "styled-components";
 import Circle from "./components/Circle";
-import {motion} from 'framer-motion';
-import {FaAngleDown} from 'react-icons/fa';
+import { motion } from "framer-motion";
+import { FaAngleDown } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import About from './About';
-import Connect from './Connect';
+import About from "./About";
+import Connect from "./Connect";
+import Project from "./Project";
 
-
-export const HomeDiv =styled(motion.div)`
-    width:90%;
-    margin: 0 auto;
-    padding-top: 150px;
-    display: flex;
-    justify-content:space-around;
-    grid-template-columns: 1fr;
-    @media all and (min-width:480px) and (max-width:767px) {
-      flex-direction: column;
-    } 
-    @media all and (max-width:479px) {
-      flex-direction: column;
-    }
-`
+export const HomeDiv = styled(motion.div)`
+  width: 90%;
+  margin: 0 auto;
+  padding-top: 150px;
+  display: flex;
+  justify-content: space-around;
+  grid-template-columns: 1fr;
+  @media all and (min-width: 480px) and (max-width: 767px) {
+    flex-direction: column;
+  }
+  @media all and (max-width: 479px) {
+    flex-direction: column;
+  }
+`;
 
 const HelloDiv = styled.div`
-    display:flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
-const CircleDiv=styled.div`
-    padding:10px; 
-    margin-bottom : 50px;
-    @media all and (min-width:480px) and (max-width:767px) {
-        margin: 50px;
-        flex-direction: column;
-        
-        height:40%;
-    } 
-    @media all and (max-width:479px) {
-        margin: 50px 0px;
-        flex-direction: column;
-        
-        height:40%;
-    }
-`
+const CircleDiv = styled.div`
+  padding: 10px;
+  margin-bottom: 50px;
+  @media all and (min-width: 480px) and (max-width: 767px) {
+    margin: 50px;
+    flex-direction: column;
+
+    height: 40%;
+  }
+  @media all and (max-width: 479px) {
+    margin: 50px 0px;
+    flex-direction: column;
+
+    height: 40%;
+  }
+`;
 
 const HelloSpan = styled.div`
-    font-size:18px;
-
-`
+  font-size: 18px;
+`;
 
 const IntroduceDiv = styled.div`
-    font-size: 100px;
-    font-weight: bold;
-    margin-bottom: 20px;
-    @media all and (min-width:480px) and (max-width:767px) {
-        font-size: 48px;
-    } 
-    @media all and (max-width:479px) {
-        font-size: 48px;
-    }
+  font-size: 100px;
+  font-weight: bold;
+  margin-bottom: 20px;
+  @media all and (min-width: 480px) and (max-width: 767px) {
+    font-size: 48px;
+  }
+  @media all and (max-width: 479px) {
+    font-size: 48px;
+  }
+`;
 
-`
-
-export const BtnDiv= styled.div`
-    width:200px;
-    height:50px;
-    display: flex;
-`
+export const BtnDiv = styled.div`
+  width: 200px;
+  height: 50px;
+  display: flex;
+`;
 
 export const HelloBtn = styled.div`
-    font-size : 24px;
-    margin: 20px 10px 0px 0px;
-    transition: transform 250ms;
-    cursor: pointer;
-    :hover{
-        transform: translateY(-3px);
-    }
-`
+  font-size: 24px;
+  margin: 20px 10px 0px 0px;
+  transition: transform 250ms;
+  cursor: pointer;
+  :hover {
+    transform: translateY(-3px);
+  }
+`;
 
-export const Slash =styled.div`
-    font-size : 24px;
-    margin: 20px 10px 0px 0px;
-`
+export const Slash = styled.div`
+  font-size: 24px;
+  margin: 20px 10px 0px 0px;
+`;
 
-const NextDiv= styled.div`
-    display: flex;
-    justify-items: center;
-    font-size: 64px;
-`
+const NextDiv = styled.div`
+  display: flex;
+  justify-items: center;
+  font-size: 64px;
+`;
 
 const ResumeA = styled.a`
-    width:200px;
-    height: 50px;
-    margin-top: 20px;
-`
+  width: 200px;
+  height: 50px;
+  margin-top: 20px;
+`;
 const ResumeBtn = styled.button`
-    width:200px;
-    height: 50px;
-    border-radius: 10px;
-    
-    border:none;
-    font-size:16px;
-    color:white;
-    cursor: pointer;
-    background-color: #707070;
-    :hover{
-        transform: scale(1.05);
-        transition-duration: 1s;
-        background-color: ${(props)=>props.theme.fontColor};
-        color:${(props)=>props.theme.bgColor}
-    }
-`
+  width: 200px;
+  height: 50px;
+  border-radius: 10px;
 
-function Home(){
-    return(
-        <>
-            <Nav/>
-            <HomeDiv 
-                transition= { {duration: 0.5 }}
-                initial= { {opacity: 0, y: 400 }}
-                animate= { {opacity: 1, y: 0 }}
-                exit= { {opacity: 0, y: -400} }  
-            >
-                <HelloDiv>
-                    <IntroduceDiv>Hello, <br/> I'm Jihoon  <br/> Lee</IntroduceDiv>
-                    <HelloSpan>Junior Front-End Developer </HelloSpan>
-                    <BtnDiv>
-                        <HelloBtn>
-                            <a href="https://www.notion.so/Full-Stack-Developer-52ce9ef0ba8d4a3583ac46a5787dd888"  rel="noopener noreferrer" target="_blank">NOTION</a> 
-                        </HelloBtn>
-                        <Slash>
-                        ｜
-                        </Slash>
-                        <HelloBtn>
-                            <a href="https://github.com/jiji-hoon96"  rel="noopener noreferrer" target="_blank">GITHUB</a>
-                        </HelloBtn>
-                    </BtnDiv>
-                    <ResumeA>
-                        <ResumeBtn>DownLoad Resume</ResumeBtn>
-                    </ResumeA>
-                </HelloDiv>
-                <CircleDiv>
-                    <Circle/>
-                </CircleDiv>
-            </HomeDiv>
-            <About/>
-            <Connect/>
-            <Link to="/project">
-                <NextDiv>
-                    <FaAngleDown/>
-                </NextDiv>
-            </Link>
-        </>
-        
-    )
+  border: none;
+  font-size: 16px;
+  color: white;
+  cursor: pointer;
+  background-color: #707070;
+  :hover {
+    transform: scale(1.05);
+    transition-duration: 1s;
+    background-color: ${(props) => props.theme.fontColor};
+    color: ${(props) => props.theme.bgColor};
+  }
+`;
+
+function Home() {
+  return (
+    <>
+      <Nav />
+      <HomeDiv
+        transition={{ duration: 0.5 }}
+        initial={{ opacity: 0, y: 400 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -400 }}
+      >
+        <HelloDiv>
+          <IntroduceDiv>
+            Hello, <br /> I'm Jihoon <br /> Lee
+          </IntroduceDiv>
+          <HelloSpan>Junior Front-End Developer </HelloSpan>
+          <BtnDiv>
+            <HelloBtn>
+              <a
+                href="https://www.notion.so/Full-Stack-Developer-52ce9ef0ba8d4a3583ac46a5787dd888"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                NOTION
+              </a>
+            </HelloBtn>
+            <Slash>｜</Slash>
+            <HelloBtn>
+              <a
+                href="https://github.com/jiji-hoon96"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                GITHUB
+              </a>
+            </HelloBtn>
+          </BtnDiv>
+          <ResumeA>
+            <ResumeBtn>DownLoad Resume</ResumeBtn>
+          </ResumeA>
+        </HelloDiv>
+        <CircleDiv>
+          <Circle />
+        </CircleDiv>
+      </HomeDiv>
+      <Project />
+      <About />
+      <Connect />
+      <Link to="/project">
+        <NextDiv>
+          <FaAngleDown />
+        </NextDiv>
+      </Link>
+    </>
+  );
 }
 
 export default Home;
--- src/Routes/Project.jsx
+++ src/Routes/Project.jsx
@@ -1,37 +1,64 @@
-import Nav from "./components/Nav"
-import { HomeDiv } from "./Home"
-import { Link } from "react-router-dom"
-import {FaAngleDown} from 'react-icons/fa';
-import styled from "styled-components"
+import Nav from "./components/Nav";
+import { HomeDiv } from "./Home";
+import { Link } from "react-router-dom";
+import { FaAngleDown } from "react-icons/fa";
+import styled from "styled-components";
 
-const NextDiv= styled.div`
-    display: flex;
-    justify-items: center;
-    font-size: 64px;
-`
+const NextDiv = styled.div`
+  display: flex;
+  justify-items: center;
+  font-size: 64px;
+`;
 
-function Project(){
-    return (
-        <>
-        <Nav/>
-        <HomeDiv
-            transition= { {duration: 0.5 }}
-            initial= { {opacity: 0, y: 400 }}
-            animate= { {opacity: 1, y: 0 }}
-            exit= { {opacity: 0, y: -400} }  
-        >
-        <h1>Project Page</h1>
-        <p>
-        프로젝트 페이지이이이이이이이이잉이ㅣㅇ
-        </p>
-        </HomeDiv>
-        <Link to="/about">
-            <NextDiv>
-                <FaAngleDown/>
-            </NextDiv>
-        </Link>
-        </>
-    )
+const ProjectDiv = styled.div`
+  width: 500px;
+  height: 600px;
+  background-color: yellow;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const ProjectDivTop = styled.div`
+  height: 50%;
+`;
+const ProjectDivBottom = styled.div`
+  height: 50%;
+`;
+const ProjectTitle = styled.div`
+  font-size: 24px;
+`;
+const ProjectSummary = styled.div``;
+const ProjectPeriod = styled.div``;
+const ProjectIcon = styled.div``;
+function Project() {
+  return (
+    <>
+      <Nav />
+      <HomeDiv
+        transition={{ duration: 0.5 }}
+        initial={{ opacity: 0, y: 400 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -400 }}
+      >
+        <ProjectDiv>
+          <ProjectDivTop>이미지자리</ProjectDivTop>
+          <ProjectDivBottom>
+            <ProjectTitle>Project : JiCoin</ProjectTitle>
+            <ProjectSummary>
+              가상화폐를 자세히 알아볼수 있는 사이트
+            </ProjectSummary>
+            <ProjectPeriod>Develop Period : 2022-5-4 ~ 2022-6-22</ProjectPeriod>
+            <ProjectIcon>Develop Tool : typescript, react</ProjectIcon>
+          </ProjectDivBottom>
+        </ProjectDiv>
+      </HomeDiv>
+      <Link to="/about">
+        <NextDiv>
+          <FaAngleDown />
+        </NextDiv>
+      </Link>
+    </>
+  );
 }
 
-export default Project
+export default Project;
